4f2cf4943642dcb5df0125ae696b048e59b3bbbb
Manuel Martínez <manuel.mtzv816@gmail.com>
2023-11-07 21:44:44 -0600

    feat: add some interaction changes to node module

--- src/views/protected/NoteView.vue
+++ src/views/protected/NoteView.vue
@@ -5,18 +5,18 @@ import useNote from "@/composables/notes/useNote";
 import Loading from "vue-loading-overlay";
 
 const route = useRoute();
-const { data: note, isFetching } = useNote({
+const { data: note, isLoading } = useNote({
   id: route.params.id as string,
 });
 </script>
 
 <template>
-  <div v-if="!isFetching && note?.data" class="py-6 mb-4">
+  <div v-if="!isLoading && note?.data" class="py-6 mb-4">
     <h2 class="text-xl font-semibold mb-4">Note View</h2>
 
     <!-- Edit form -->
     <NoteEditForm :note="note.data" />
   </div>
 
-  <Loading v-model:active="isFetching" />
+  <Loading v-model:active="isLoading" />
 </template>
